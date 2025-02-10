403c4838ab199469b30234d8a1a9b60b4c434921
San231926 <san231926@uvg.edu.gt>
2025-02-09 19:29:20 -0600

    InsertionSort

--- src/main/java/uvg.edu.gt/InsertionSort.java
+++ src/main/java/uvg.edu.gt/InsertionSort.java
@@ -1,15 +1,17 @@
 package uvg.edu.gt;
 
+import java.util.*;
+
 public class InsertionSort {
     public static void sort(int[] numbers) {
-        for (int i = 1; i < numbers.size(); i++) {
-            int key = numbers.get(i);
+        for (int i = 1; i < numbers.length; i++) {
+            int key = numbers[i];
             int j = i - 1;
-            while (j >= 0 && numbers.get(j) > key) {
-                numbers.set(j + 1, numbers.get(j));
+            while (j >= 0 && numbers[j] > key) {
+                numbers[j + 1] = numbers[j];
                 j--;
             }
-            numbers.set(j + 1, key);
+            numbers[j + 1] = key;
         }
     }
 }
